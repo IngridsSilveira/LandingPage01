1a3d60138a1efd272852ebd61c85765e312d27bd
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-09-16 15:14:43 -0300

    finalizado

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,14 @@ const input = document.querySelector("[data-input]");
 const button = document.querySelector("[data-button]");
 let ArrayDeEmails = [];
 
-//ESTA FUNÇÃO É PARA PEGAR OS EMAILS DIGITADOS NO INPUT//
+//ESTÁ FUNÇÃO É PARA PEGAR OS EMAILS DIGITADOS NO INPUT//
 button.addEventListener("click", (evento)=>{
     evento.preventDefault();
     ArrayDeEmails.push(input.value);
     console.log(ArrayDeEmails);
 })
 
-//ESTA FUNÇÃO É PARA O BOTÃO NO FINAL DA PÁGINA//
+//ESTÁ FUNÇÃO É PARA O BOTÃO NO FINAL DA PÁGINA//
 function aviso(){
     alert("Suba e preencha o seu e-mail!!");
 }
